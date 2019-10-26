060bf3ba03daeb6251c3b4766ea9dd9422619292
ulquiola <ulquiola@163.com>
2019-10-26 21:51:11 +0800

    add Md5Encryption

--- PurchaSaler/PurchaSaler/Controllers/AccountController.cs
+++ PurchaSaler/PurchaSaler/Controllers/AccountController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using BLL;
 using Models;
-using BLL;
+using PurchaSaler.Tools;
+using System.Web.Mvc;
 
 namespace PurchaSaler.Controllers
 {
     public class AccountController : Controller
     {
+        UsersManager usersManager = new UsersManager();
+
         // GET: Account
         public ActionResult Index()
         {
@@ -24,13 +23,17 @@ namespace PurchaSaler.Controllers
         [HttpPost]
         public ActionResult Register(Users man)
         {
-            UsersManager usersManager = new UsersManager();
-            Users info = usersManager.GetUserInfo(man.UserName);
+            //判断用户是否存在
             if (usersManager.UserIsExited(man.UserName))
             {
-                return Content("用户已存在");
+                return Content("用户已存在，请更换");
             }
-            return Content("用户不存在");
+            //判断两次密码是否一致
+            //md5加密
+            MD5Encryption md5 = new MD5Encryption();
+            string hash = md5.GetMd5Hash(man.PassWord);
+            return Content(hash);
         }
+
     }
 }
---
+++ PurchaSaler/PurchaSaler/Tools/MD5Encryption.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PurchaSaler.Tools
+{
+    public class MD5Encryption
+    {
+        public string GetMd5Hash(string password)
+        {
+            StringBuilder targetStr = new StringBuilder();
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+            int length = data.Length;
+            for (int i=0;i<length;i++)
+            {
+                targetStr.Append(data[i].ToString("X2"));
+            }
+            return targetStr.ToString();
+        }
+    }
+}
